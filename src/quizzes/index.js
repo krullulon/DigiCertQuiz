30c3507657c08d742c2614716a39f46751e2a559
krullulon <jasonmpace@gmail.com>
2025-10-22 14:30:37 -0700

    Pushing week 2 but not changing base domain yet

--- src/quizzes/index.js
+++ src/quizzes/index.js
@@ -1,7 +1,9 @@
 import week1 from "./week-1-key-sovereignty";
+import week2 from "./week-2-x9-extended-key-usage";
 
 export const quizzes = {
   [week1.id]: week1,
+  [week2.id]: week2,
 };
 
 export const currentQuizId = week1.id;
@@ -9,4 +11,3 @@ export const currentQuizId = week1.id;
 export function getQuiz(id) {
   return quizzes[id];
 }
-
