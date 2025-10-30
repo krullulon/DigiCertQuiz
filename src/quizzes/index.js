312a2e89974269cd213483e99d2d61966f7cb4f8
krullulon <jasonmpace@gmail.com>
2025-10-30 11:49:17 -0700

    updating current quiz to default url

--- src/quizzes/index.js
+++ src/quizzes/index.js
@@ -1,12 +1,14 @@
 import week1 from "./week-1-key-sovereignty";
 import week2 from "./week-2-x9-extended-key-usage";
+import week3 from "./week-3-protocols";
 
 export const quizzes = {
   [week1.id]: week1,
   [week2.id]: week2,
+  [week2.id]: week3,
 };
 
-export const currentQuizId = week2.id;
+export const currentQuizId = week3.id;
 
 export function getQuiz(id) {
   return quizzes[id];
